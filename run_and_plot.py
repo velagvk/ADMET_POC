1f3678061ecdcba1dda6ab1340d4c9fca358f924
velagvk <139946537+velagvk@users.noreply.github.com>
2023-07-23 22:13:39 -0500

    Update run_and_plot.py

--- run_and_plot.py
+++ run_and_plot.py
@@ -8,6 +8,5 @@ import train
 import plot_predictions
 import sys
 path=sys.argv[1]
-
-tes = train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,
-    batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=140, N=5000 ,path)  
+def run_and_plot(path):
+    train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=140, N=5000 ,path)  
