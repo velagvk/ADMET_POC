c87bf0edb727a440cc7d911c3a0c28ed466476a0
velagvk <139946537+velagvk@users.noreply.github.com>
2023-07-23 22:27:09 -0500

    Update train.py

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def train (test_name, radius=1, dim=64, layer_hidden=4, layer_output=10, dropout
                     tester.save_MAEs(result , file_MAEs )
                     file_model = path+ '/output/'+ 'model/' + 'model' + '.h5'
                     tester.save_model(model, file_model)     
-        return result
+              return result
 
 
 
