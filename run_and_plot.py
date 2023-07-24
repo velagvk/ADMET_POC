6cb25b8d08540644ae200066698545798b091f4f
velagvk <139946537+velagvk@users.noreply.github.com>
2023-07-23 21:46:30 -0500

    Update run_and_plot.py

--- run_and_plot.py
+++ run_and_plot.py
@@ -8,4 +8,4 @@ import train
 import plot_predictions
 
 tes = train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,
-    batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=140, N=5000 ,path,dataname='Caco2_Wang')  
+    batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=140, N=5000 ,path)  
