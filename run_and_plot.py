ce775a84349ca28bc246f76da27ff69acb253d5d
velagvk <139946537+velagvk@users.noreply.github.com>
2023-07-24 21:00:36 -0500

    Update run_and_plot.py

--- run_and_plot.py
+++ run_and_plot.py
@@ -9,4 +9,4 @@ import sys
 folder=sys.argv[1]
 train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=50, N=5000,path=folder)  
 file_maes=folder + '/MAEs.txt'
-plt_pred.plot_training_loss(file_maes)
+plot_pred.plot_training_loss(file_maes)
