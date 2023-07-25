29b0e5af79302279cecf7485325d350e71044cf9
velagvk <139946537+velagvk@users.noreply.github.com>
2023-07-25 00:03:54 -0500

    Update run_and_plot.py

--- run_and_plot.py
+++ run_and_plot.py
@@ -9,5 +9,5 @@ import sys
 folder=sys.argv[1]
 dataname =sys.argv[2]
 print(dataname)
-train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=50, N=5000,path=folder,data=dataname)  
+train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=100, N=5000,path=folder,data=dataname)  
 plot_pred.plot_training_loss(folder)
