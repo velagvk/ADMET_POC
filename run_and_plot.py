38b8fc666ca3350f6e9396d2f583e2698419c92f
velagvk <139946537+velagvk@users.noreply.github.com>
2023-07-24 20:46:53 -0500

    Update run_and_plot.py

--- run_and_plot.py
+++ run_and_plot.py
@@ -4,7 +4,9 @@ import train
 import numpy as np
 import rdkit
 import train
-import plot_predictions
+import plot_predictions as plot_pred
 import sys
 folder=sys.argv[1]
-train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=140, N=5000,path=folder)  
+train.train(radius=1, dim=64, layer_hidden=4, layer_output=10, dropout=0.45, batch_train=8,batch_test=8, lr=1e-4, lr_decay=0.85, decay_interval=25, iteration=50, N=5000,path=folder)  
+file_maes=folder + '/MAEs.txt'
+plt_pred.plot_training_loss(file_maes)
